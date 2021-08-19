a645e7713b310e777e87057f77482d51846fbb02
hepimen <mhrzstudio@gmail.com>
2021-08-19 15:37:26 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="container">
+      <div class="row header mb-5 p-3">
+        <div class="col-lg-12">
+          <h2>What i do today</h2>
+        </div>
+      </div>
+      <div class="row content">
+        <div class="col-md-4">
+          <div class="task-home">
+            <div class="row">
+              <div
+                v-for="myTask in task"
+                v-bind:key="myTask.id"
+                class="task mb-3"
+              >
+                <ItemTask v-bind:myTask="myTask" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.css";
+import ItemTask from "./components/ItemTask.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data: function () {
+    return {
+      task: [
+        {
+          id: 1,
+          taskName: "Membersihkan Kamar",
+          isDone: false,
+        },
+        {
+          id: 2,
+          taskName: "Memanggang Daging",
+          isDone: false,
+        },
+        {
+          id: 3,
+          taskName: "Lari Pagi",
+          isDone: false,
+        },
+        {
+          id: 4,
+          taskName: "Istirahat cukup",
+          isDone: false,
+        },
+      ],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    ItemTask,
+  },
+};
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +71,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.header {
+  background: rgba(125, 162, 197, 0.8);
+}
+.task .item {
+  font-size: 20px;
+}
+.task-home {
+  text-align: left;
+}
+.item-completed {
+  color: rgb(8, 180, 8);
 }
 </style>
---
+++ src/components/ItemTask.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="form-check">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        value=""
+        id="defaultCheck1"
+        onclick=""
+      />
+      <label
+        class="form-check-label item"
+        for="defaultCheck1"
+        v-bind:class="{ 'item-completed': myTask.isDone }"
+      >
+        {{ myTask.taskName }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "myTask",
+  props: ["myTask"],
+};
+</script>
+
+<style scoped>
+</style>
